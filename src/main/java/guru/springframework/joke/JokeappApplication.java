6bb902541f321776d54652e8f858fed1e295e7f7
111sunil <SuniLch@111>
2020-12-25 16:21:04 +0530

    Spring Multiple Properties files demo.

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.joke;
 
 import guru.springframework.joke.examplebeans.FakeDataSource;
+import guru.springframework.joke.examplebeans.FakeJmsBroker;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +18,10 @@ public class JokeappApplication {
         FakeDataSource fakeDataSource = (FakeDataSource) context.getBean(FakeDataSource.class);
 
         System.out.println(fakeDataSource.getUser());
+
+        FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) context.getBean(FakeJmsBroker.class);
+
+        System.out.println(fakeJmsBroker.getUsername());
     }
 
 }
