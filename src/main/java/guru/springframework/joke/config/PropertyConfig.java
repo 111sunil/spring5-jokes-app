848371db6668907d742ca3e682a1e07b98f95108
111sunil <SuniLch@111>
2020-12-25 15:48:10 +0530

    Spring Environment Variables Demo.

--- src/main/java/guru/springframework/joke/config/PropertyConfig.java
+++ src/main/java/guru/springframework/joke/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package guru.springframework.joke.config;
 
 import guru.springframework.joke.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${guru.username}")
     String user;
@@ -23,7 +28,8 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        //fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
